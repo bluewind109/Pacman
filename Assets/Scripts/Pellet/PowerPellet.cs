d6fd8be33354f3da09a0ce9dd6bc60e4ac596150
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2026-04-24 21:32:55 +0700

    - Remove consume function

--- Assets/Scripts/Pellet/PowerPellet.cs
+++ Assets/Scripts/Pellet/PowerPellet.cs
@@ -3,10 +3,4 @@ using UnityEngine;
 public class PowerPellet : Pellet
 {
     [SerializeField] private float duration = 8f;
-
-    protected override void Consume()
-    {
-        base.Consume();
-        // StartCoroutine(FindObjectOfType<GameManager>().PowerPelletEffect(duration));
-    }
 }
